5ad912414d9cbde398d8f49b4ad33f3832fc9b52
Unibit <emilsvilcins@uni-bit.org>
2025-12-16 09:57:48 +0200

    Started making model for DB Connections

---
+++ model/condb.php
@@ -0,0 +1,19 @@
+<?php
+function getDBSettings() {
+	$dbsettingsFile = fopen("dbsettings.json") or return "Failed to open file!";
+	$fdata = fread($dbsettingsFile, filesize("dbsettings.json"));
+	return $fdata;
+}
+
+function conDB() {
+	$dbData = json_decode(getDBSettings());
+	
+	$conn = new mysqli($dbData["host"], $dbData["user"], $dbData["pass"], $dbData["db"]);
+
+	if ($conn->connect_error)
+	{
+		$conn->close();
+		die("Issue connecting to the database! Check config!");		
+	}
+}
+?>
